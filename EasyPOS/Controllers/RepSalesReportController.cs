08930e02e11cd32609e5aa6cf79039dc124fb2ae
eganpido <30331974+eganpido@users.noreply.github.com>
2022-03-30 15:54:45 +0800

    Bug fix when generating CSV in Sales Summary Report filtered by Customer

--- EasyPOS/Controllers/RepSalesReportController.cs
+++ EasyPOS/Controllers/RepSalesReportController.cs
@@ -114,8 +114,8 @@ namespace EasyPOS.Controllers
                                 Customer = d.MstCustomer.Customer,
                                 Term = d.MstTerm.Term,
                                 Remarks = d.Remarks,
-                                PreparedByUserName = d.MstUser.UserName,
-                                UpdatedUserName = d.MstUser4.UserName,
+                                PreparedByUserName = d.MstUser.FullName,
+                                UpdatedUserName = d.MstUser4.FullName,
                                 Amount = d.Amount,
                                 EntryDateTime = d.EntryDateTime.ToShortTimeString()
 
@@ -143,6 +143,7 @@ namespace EasyPOS.Controllers
                                 Term = d.MstTerm.Term,
                                 Remarks = d.Remarks,
                                 PreparedByUserName = d.MstUser.FullName,
+                                UpdatedUserName = d.MstUser4.FullName,
                                 Amount = d.Amount,
                                 EntryDateTime = d.EntryDateTime.ToShortTimeString()
 
